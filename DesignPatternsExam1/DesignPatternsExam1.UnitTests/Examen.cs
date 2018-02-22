2c7473cad20916cd44daa96d542ebc5d521a818a
EX2OUTCODING\mtorres <mtorres@outcoding.com>
2018-02-21 18:53:10 -0600

    Adding more validation to the test

--- DesignPatternsExam1/DesignPatternsExam1.UnitTests/Examen.cs
+++ DesignPatternsExam1/DesignPatternsExam1.UnitTests/Examen.cs
@@ -21,6 +21,12 @@ namespace DesignPatternsExam1.UnitTests
             Assert.AreEqual(1, ProcesadorRomano.Convierte("I"));
             Assert.AreEqual(2, ProcesadorRomano.Convierte("II"));
             Assert.AreEqual(3, ProcesadorRomano.Convierte("III"));
+            Assert.AreEqual(5, ProcesadorRomano.Convierte("V"));
+            Assert.AreEqual(10, ProcesadorRomano.Convierte("X"));
+            Assert.AreEqual(50, ProcesadorRomano.Convierte("L"));
+            Assert.AreEqual(100, ProcesadorRomano.Convierte("C"));
+            Assert.AreEqual(500, ProcesadorRomano.Convierte("D"));
+            Assert.AreEqual(1000, ProcesadorRomano.Convierte("M"));
             ProcesadorRomano.Convierte("IIII");
         }
     }
